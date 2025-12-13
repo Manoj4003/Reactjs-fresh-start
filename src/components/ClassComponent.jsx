e35a44fb89a4ed1bbe7e18822e92f720dfe19536
Manoj4003 <manojknr16@gmail.com>
2025-12-13 23:00:16 +0530

    Types of Components Intro

---
+++ src/components/ClassComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function ClassComponent() {
+  return (
+   <ul>
+<li>id:101</li>
+<li>name:Manoj</li>
+<li>age:24</li>
+
+
+   </ul>
+  )
+}
---
+++ src/components/StudentComponent.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function StudentComponent() {
+  return (
+  <ul>
+
+    <li>Id : 101</li>
+    <li>Name : Manoj</li>
+    <li>Age : 24</li>
+  </ul>
+  )
+}
